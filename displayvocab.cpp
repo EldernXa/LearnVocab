60f6fcb4e75b254c85ac480a3e017fe1f6fe0725
anthony <anthony_pro1998@outlook.fr>
2021-06-30 15:37:04 +0200

    save numberofcolumn from vocab

--- displayvocab.cpp
+++ displayvocab.cpp
@@ -2,5 +2,31 @@
 
 DisplayVocab::DisplayVocab(std::string nameVocab, QWidget *parent) : QMainWindow(parent)
 {
+    nameVocabToDisplay = nameVocab;
+    scrollArea = new QScrollArea(this);
+    QWidget *scrollAreaContent = new QWidget;
+    scrollAreaContent->setLayout(new QVBoxLayout);
+    scrollArea->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    scrollArea->setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+    scrollArea->setWidgetResizable(true);
+    scrollArea->setWidget(scrollAreaContent);
 
+    fileToDisplayVocab.open(nameVocab+".vocab", ios::in);
+    string value;
+    getline(fileToDisplayVocab, value);
+    numberOfColumn = stoi(value);
+    cout << numberOfColumn << endl;
+    fileToDisplayVocab.close();
 }
+
+
+
+
+
+
+
+
+
+
+
+
--- displayvocab.h
+++ displayvocab.h
@@ -1,13 +1,30 @@
 #ifndef DISPLAYVOCAB_H
 #define DISPLAYVOCAB_H
 
+#include <fstream>
+#include <iostream>
+
 #include <QMainWindow>
+#include <QScrollArea>
+#include <QVBoxLayout>
+
+using namespace std;
+
 
 class DisplayVocab : public QMainWindow
 {
     Q_OBJECT
 public:
     explicit DisplayVocab(std::string nameVocab, QWidget *parent = nullptr);
+
+
+private:
+    string nameVocabToDisplay;
+    int numberOfColumn;
+
+    fstream fileToDisplayVocab;
+
+    QScrollArea *scrollArea;
 
 signals:
 

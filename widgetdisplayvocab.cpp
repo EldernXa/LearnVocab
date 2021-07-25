9c708415bc37d2e28e8cdcf427cb3ac0895a9cd4
anthony <anthony_pro1998@outlook.fr>
2021-07-25 11:14:57 +0200

    remove all comments for now in displayvocab class

--- widgetdisplayvocab.cpp
+++ widgetdisplayvocab.cpp
@@ -7,100 +7,23 @@ WidgetDisplayVocab::WidgetDisplayVocab(std::string nameVocab, QWidget *parent) :
 {
     ui->setupUi(this);
     nameVocabToDisplay = nameVocab;
-//    QWidget *scrollAreaContent = new QWidget;
-//    ui->scrollArea->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-//    ui->scrollArea->setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
-//    ui->scrollArea->setWidgetResizable(true);
-//    ui->scrollArea->setWidget(scrollAreaContent);
     readerVocab = new ReaderVocab(nameVocab);
-    //fileToDisplayVocab.open(nameVocab+".vocab", ios::in);
-//    string value;
-//    getline(fileToDisplayVocab, value);
-//    numberOfColumn = stoi(value);
-//    getline(fileToDisplayVocab, value);
-//    nameColumns = split(value, ';');
-
-//    this->resize(numberOfColumn*WIDTH_LABEL + WIDTH_LABEL, 400);
     this->resize(readerVocab->getNumberOfColumns()*WIDTH_LABEL + WIDTH_LABEL, 400);
     ui->widget->setLayout(new QVBoxLayout);
     QWidget *widgetForNameColumns = new QWidget;
     ui->widget->layout()->addWidget(widgetForNameColumns);
     QHBoxLayout *layoutForNameColumns = new QHBoxLayout(widgetForNameColumns);
-//    for(auto &nameColumn : nameColumns){
-//        QLabel *lblNameColumn = new QLabel;
-//        lblNameColumn->setText(QString::fromStdString(nameColumn));
-//        lblNameColumn->setAlignment(Qt::AlignCenter);
-//        //lblNameColumn->setFixedHeight(HEIGHT_LABEL);
-//        //lblNameColumn->setFixedWidth(WIDTH_LABEL);
-//        layoutForNameColumns->addWidget(lblNameColumn);
-//    }
     for(auto &nameColumn : readerVocab->getColumnName()){
         QLabel *lblNameColumn = new QLabel;
         lblNameColumn->setText(QString::fromStdString(nameColumn));
         lblNameColumn->setAlignment(Qt::AlignCenter);
-        //lblNameColumn->setFixedHeight(HEIGHT_LABEL);
-        //lblNameColumn->setFixedWidth(WIDTH_LABEL);
         layoutForNameColumns->addWidget(lblNameColumn);
     }
     lastHeight += HEIGHT_LABEL+1;
     QFrame *frameVLine = new QFrame;
     ui->widget->layout()->addWidget(frameVLine);
-    //frameVLine->setFixedWidth(numberOfColumn*WIDTH_LABEL);
     frameVLine->setFrameShape(QFrame::HLine);
     frameVLine->move(0, lastHeight);
-//    while(getline(fileToDisplayVocab, value)){
-//        bool verifIsKnown = split(value, ':').size()>1;
-//        vector<string> valueToDisplay = split(split(value, ':').at(0), ';');
-//        QWidget *widgetForNameWord = new QWidget;
-//        ui->widget->layout()->addWidget(widgetForNameWord);
-//        QHBoxLayout *layoutForNameWord = new QHBoxLayout(widgetForNameWord);
-//        widgetForNameWord->move(0, lastHeight);
-//        unsigned int multiply_value = 1;
-//        for(auto &nameWord : valueToDisplay){
-//            vector<string> differentValueForAWord = split(nameWord, ',');
-//            if(differentValueForAWord.size()>1){
-//                QWidget *widgetForAWord = new QWidget;
-//                QVBoxLayout *layoutForAWord = new QVBoxLayout(widgetForAWord);
-//                layoutForAWord->setSpacing(0);
-//                if(differentValueForAWord.size()>multiply_value){
-//                    multiply_value = differentValueForAWord.size();
-//                }
-//                for(auto &word : differentValueForAWord){
-//                    QLabel *lblNameWord = new QLabel;
-//                    lblNameWord->setAlignment(Qt::AlignCenter);
-//                    lblNameWord->setText(QString::fromStdString(word));
-//                    //lblNameWord->setFixedHeight(HEIGHT_LABEL);
-//                    //lblNameWord->setFixedWidth(WIDTH_LABEL);
-//                    if(verifIsKnown){
-//                        lblNameWord->setStyleSheet("QLabel {color:green;}");
-//                    }else{
-//                        lblNameWord->setStyleSheet("QLabel {color:red;}");
-//                    }
-//                    layoutForAWord->addWidget(lblNameWord);
-//                }
-//                layoutForNameWord->addWidget(widgetForAWord);
-//            }else{
-//                QLabel *lblNameWord = new QLabel;
-//                lblNameWord->setAlignment(Qt::AlignCenter);
-//                lblNameWord->setText(QString::fromStdString(nameWord));
-//                //lblNameWord->setFixedHeight(HEIGHT_LABEL);
-//                //lblNameWord->setFixedWidth(WIDTH_LABEL);
-//                if(verifIsKnown){
-//                    lblNameWord->setStyleSheet("QLabel {color:green;}");
-//                }else{
-//                    lblNameWord->setStyleSheet("QLabel {color:red;}");
-//                }
-//                layoutForNameWord->addWidget(lblNameWord);
-//            }
-//        }
-//        lastHeight+=(HEIGHT_LABEL*multiply_value);
-//        QFrame *frameLine = new QFrame;
-//        ui->widget->layout()->addWidget(frameLine);
-//        //frameLine->setFixedWidth(numberOfColumn*WIDTH_LABEL);
-//        frameLine->setFrameShape(QFrame::HLine);
-//        frameLine->move(0, lastHeight);
-//    }
-    //fileToDisplayVocab.close();
 
     for (unsigned int i=0; i< readerVocab->getAllLineOfTheVocab().size(); i++){
         bool verifIsKnown = readerVocab->isLineVocabKnown(i);
--- widgetdisplayvocab.h
+++ widgetdisplayvocab.h
@@ -37,16 +37,12 @@ private:
 
     ReaderVocab *readerVocab = nullptr;
     string nameVocabToDisplay;
-    //int numberOfColumn;
     int lastHeight = 0;
-    //vector<string> nameColumns;
     const int WIDTH_LABEL = 150;
     const int HEIGHT_LABEL = 30;
     const int LIMIT_WIDGET = 5;
     const int HEIGHT_WIDGET = 30 * LIMIT_WIDGET;
     QScrollArea *scrollArea;
-
-    //fstream fileToDisplayVocab;
 
     Ui::WidgetDisplayVocab *ui;
 };
